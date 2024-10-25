a9c65f025228edd86cc0c4d0260f5e9fefa9a80b
Abimbola <ahassan@chapelhilldenham.com>
2024-10-25 17:30:08 +0100

    Woring on Saveplan and added the terms for collections

--- Backend/SavePlan/src/database/sequelize/INv2/index.ts
+++ Backend/SavePlan/src/database/sequelize/INv2/index.ts
@@ -1,7 +1,6 @@
 import { Sequelize, Op } from "sequelize";
 export { Sequelize, Op };
-export * from "./models/BvnData";
-export * from "./models/Tenant";
 export * from "./models/User";
-export * from "./models/Role";
-export * from "./models/TenantUserRole";
+export * from "./models/Media";
+export * from "./models/SavePlan";
+export * from "./models/SavePlanUser";
--- Backend/SavePlan/src/database/sequelize/INv2/models/User.ts
+++ Backend/SavePlan/src/database/sequelize/INv2/models/User.ts
@@ -1,10 +1,5 @@
-import { Model, Table, Column, DataType, HasMany, HasOne, BeforeUpdate, BeforeCreate } from "sequelize-typescript";
-// import { InferAttributes, InferCreationAttributes, CreationOptional } from 'sequelize';
-
-import { BvnData, TenantUserRole } from "../";
-import { DBEnums } from "@inv2/common";
-
-import { PasswordManager } from "../../../../main/_utils/PasswordManager";
+import { Model, Table, Column, DataType, BelongsToMany, } from "sequelize-typescript";
+import { SavePlan, SavePlanUser } from "../";
 
 @Table({
    timestamps: true,
@@ -12,13 +7,19 @@ import { PasswordManager } from "../../../../main/_utils/PasswordManager";
    underscored: true,
    paranoid: true,
 })
-// export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
 export class User extends Model {
    
-   @HasOne(() => BvnData)
-   declare bvnData: BvnData;
-   @HasMany(() => TenantUserRole)
-   declare tenantUserRoles: TenantUserRole[];
+   @BelongsToMany(() => SavePlan, () => SavePlanUser)
+   declare saveplans: SavePlan[];
+
+   @Column({ type: DataType.DATE, })
+   declare createdAt: Date;
+   @Column({ type: DataType.DATE, })
+   declare updatedAt: Date;
+   @Column({ type: DataType.DATE, })
+   declare deletedAt: Date;
+   @Column({ type: DataType.INTEGER, defaultValue: 0 })
+   declare version: number;
 
    @Column({
       primaryKey: true,
@@ -30,94 +31,26 @@ export class User extends Model {
    @Column({ type: DataType.UUID })
    declare pId: string;
 
-   @Column({ type: DataType.STRING(100), })
-   declare firstName: string;
-
-   @Column({ type: DataType.STRING(100), })
-   declare middleName: string;
-
-   @Column({ type: DataType.STRING(100), })
-   declare lastName: string;
-   
-   @Column({
-      type: DataType.STRING(13),
-      unique: true,
-   })
-   declare bvn: string;
-   @Column({
-      type: DataType.STRING(13),
-      unique: true,
-   })
-   declare nin: string;
-   @Column({
-      type: DataType.STRING(200),
-      unique: true
-   })
-   declare email: string;
-
-   @Column({ type: DataType.SMALLINT, })
-   get gender(): { code: number; name: string; label: string; } | undefined {
-      const rawValue = this.getDataValue('gender');
-      return DBEnums.UserGender.find(g=>g.code===rawValue);
+   /**
+   * Should hold a stringified list of other user data
+   */
+   @Column({type: DataType.TEXT,})
+   get details(): string {
+      return JSON.parse(this.getDataValue('details'));
+   }  
+   set details(value: string) {
+      this.setDataValue('details', JSON.stringify(value));
    }
-   set gender(value: number|string) {
-      const result = DBEnums?.UserGender?.find(g=>(g.code==value || g.label==value || g.name==value))?.code;
-      this.setDataValue('gender', result);
+   /**
+   * Should hold an array of tenant and roles a user has in tenants
+   * [{id: 123, name: "Cool tenant", roles: [{id: 1, name: "user"}]}]
+   */
+   @Column({ type: DataType.TEXT, })
+   get tenantRoles(): string {
+      return JSON.parse(this.getDataValue('tenantRoles'));
+   }  
+   set tenantRoles(value: string) {
+      this.setDataValue('tenantRoles', JSON.stringify(value));
    }
-   
-   @Column({ type: DataType.DATEONLY, })
-   declare dob: Date;
 
-   @Column({
-      type: DataType.STRING(20),
-      unique: true,
-   })
-   declare phone: string;
-   
-   @Column({ type: DataType.STRING(500), })
-   declare password: string;
-
-   @Column({
-      type: DataType.STRING(20),
-      unique: true,
-   })
-   declare refCode: string;
-
-   @Column({ type: DataType.STRING(20), })
-   declare referrer: string;
-   
-   @Column({
-      type: DataType.BOOLEAN,
-      defaultValue: true
-   })
-   declare showBalance: boolean;
-   
-   @Column({ type: DataType.STRING(100), })
-   declare mothersMaidenName: string;
-   
-   @Column({ type: DataType.STRING(100), })
-   declare placeOfBirth: string;
-   
-   @Column({ type: DataType.BOOLEAN, })
-   declare isEnabled: boolean;
-   
-   @Column({ type: DataType.BOOLEAN, })
-   declare isLocked: boolean;
-   
-   @Column({ type: DataType.BOOLEAN, })
-   declare firstLogin: boolean;
-   
-   @Column({ type: DataType.BOOLEAN, })
-   declare twoFactorAuth: boolean;
-
-   @BeforeUpdate
-   @BeforeCreate
-   static async savePassword(instance: User) {
-      if(instance.password) {
-         const hashed = await PasswordManager.toHash(instance.password);
-         instance.password = hashed;
-      }
-   }
 }
-
-// export default {User};
