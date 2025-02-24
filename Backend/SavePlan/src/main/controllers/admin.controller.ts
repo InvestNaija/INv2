6f2326b0cbfc811d7101eeddfec4a2a5ba67a8ad
Abimbola Hassan <ahassan@chapelhilldenham.com>
2025-02-24 16:46:41 +0100

    Changing exchange from direct to topic

--- Backend/SavePlan/src/main/controllers/admin.controller.ts
+++ Backend/SavePlan/src/main/controllers/admin.controller.ts
@@ -1,10 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
-import { Exception, handleError, INLogger, JoiMWDecorator } from "@inv2/common";
+import { Exception, handleError, INLogger, JoiMWDecorator, UnauthorizedError, UserTenantRoleDto } from "@inv2/common";
 
 import { AdminService } from '../services';
 import { AdminValidation } from '../validations/admin.schema';
 
 export class AdminController {
+   static authorize(roles:string[] = []) {
+      if(!Array.isArray(roles) || roles.length <= 0) throw new UnauthorizedError();
+      if(roles.length === 1 && roles.includes('*')){
+         roles = ['TENANT_ADMIN', 'SUPER_ADMIN', 'USER', 'CUSTOMER'];
+      }
+      return async (req: Request, res: Response, next: NextFunction) => {
+         try {
+
+            const userTenantRole = req?.currentUser as UserTenantRoleDto;
+            if(!userTenantRole || userTenantRole.Tenant[0]) throw new UnauthorizedError();
+            console.log(userTenantRole.Tenant[0]);
+            
+            // if(userTenantRole.Tenant[0]) throw new UnauthorizedError();
+            // const role = userTenantRole.Tenant[0]?.Role[0] as Role;
+            // if(!roles.includes(userTenantRole.Tenant[0]?.Role[0] || '')) throw new UnauthorizedError();
+         } catch (error) {
+            console.log(error);
+            next(new Exception(handleError(error)));
+         }
+      };
+   }
    public static async list(req: Request, res: Response, next: NextFunction): Promise<void> {
       const profiler = INLogger.log.startTimer();
       try {         
