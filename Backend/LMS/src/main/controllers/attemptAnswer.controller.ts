37e8c85dfe8700e2304da6963413a19ce8e656dc
AdeoluX <73498971+AdeoluX@users.noreply.github.com>
2025-02-25 10:36:06 +0100

    fix

--- Backend/LMS/src/main/controllers/attemptAnswer.controller.ts
+++ Backend/LMS/src/main/controllers/attemptAnswer.controller.ts
@@ -1,62 +1,70 @@
 import { NextFunction, Request, Response } from 'express';
 import { Exception, CustomError, JoiMWDecorator } from "@inv2/common";
-import { AttemptAnswerValidation  } from '../validations/attemptAnswer.schema';
-
-import { AttemptAnswerService} from '../services';
+import { AttemptAnswerValidation } from '../validations/attemptAnswer.schema';
+import { AttemptAnswerService } from '../services';
 import { AttemptAnswerDto, GetAttemptAnswerDto } from '../dtos';
 
-
 export class AttemptAnswerController {
-
-   // private static attemptAnswerService = new AttemptAnswerService()
+   private static attemptAnswerService = new AttemptAnswerService();
 
    public static async healthz(req: Request, res: Response): Promise<void> {
-      res.status(200).json({status:200, message: "Auth server is Healthy"});
+      res.status(200).json({ status: 200, message: "Auth server is Healthy" });
    }
 
    @JoiMWDecorator(AttemptAnswerValidation.attemptAnswer)
    public static async attemptAnswer(req: Request, res: Response, next: NextFunction): Promise<void> {
       try {
-         const attemptAnswer =  new  AttemptAnswerService      
          const body: AttemptAnswerDto = req.body;
-         const attemptAnswerService = await attemptAnswer.attemptAnswer( req.currentUser!, body );
-         res.status(attemptAnswerService.code).json(attemptAnswerService);
-      } catch (error: unknown|Error) {
-         if(error instanceof CustomError) next( new Exception(error));
+         const response = await AttemptAnswerController.attemptAnswerService.attemptAnswer(req.currentUser!, body);
+         res.status(response.code).json(response);
+      } catch (error: unknown) {
+         if (error instanceof CustomError) {
+            return next(new Exception(error));
+         }
+         return next(error);
       }
    }
+
    @JoiMWDecorator(AttemptAnswerValidation.getAttemptAnswer)
    public static async getAttemptAnswer(req: Request, res: Response, next: NextFunction): Promise<void> {
       try {
-         const attemptAnswer =  new  AttemptAnswerService
          const query: Partial<GetAttemptAnswerDto> = req.query;
-         const attemptAnswerService = await attemptAnswer.getAttemptAnswer(query)
-         res.status(attemptAnswerService.code).json(attemptAnswerService);
-      } catch (error) {
-         if(error instanceof CustomError) next( new Exception(error));
+         const response = await AttemptAnswerController.attemptAnswerService.getAttemptAnswer(query);
+         res.status(response.code).json(response);
+      } catch (error: unknown) {
+         if (error instanceof CustomError) {
+            return next(new Exception(error));
+         }
+         return next(error);
       }
    }
+
    @JoiMWDecorator(AttemptAnswerValidation.updateAttemptAnswer)
    public static async updateAttemptAnswer(req: Request, res: Response, next: NextFunction): Promise<void> {
       try {
-         const attemptAnswer =  new  AttemptAnswerService
          const { id } = req.params;
          const data = req.body;
-         const attemptAnswerService = await attemptAnswer.updateAttemptAnswer(id, data)
-         res.status(attemptAnswerService.code).json(attemptAnswerService);
-      } catch (error) {
-         if(error instanceof CustomError) next( new Exception(error));
+         const response = await AttemptAnswerController.attemptAnswerService.updateAttemptAnswer(id, data);
+         res.status(response.code).json(response);
+      } catch (error: unknown) {
+         if (error instanceof CustomError) {
+            return next(new Exception(error));
+         }
+         return next(error);
       }
    }
+
    @JoiMWDecorator(AttemptAnswerValidation.deleteAttemptAnswer)
    public static async deleteAttemptAnswer(req: Request, res: Response, next: NextFunction): Promise<void> {
       try {
-         const attemptAnswer =  new  AttemptAnswerService
-         const id = req.params.id;
-         const attemptAnswerService = await attemptAnswer.deleteAttemptAnswer(id)
-         res.status(attemptAnswerService.code).json(attemptAnswerService);
-      } catch (error) {
-         if(error instanceof CustomError) next( new Exception(error));
+         const { id } = req.params;
+         const response = await AttemptAnswerController.attemptAnswerService.deleteAttemptAnswer(id);
+         res.status(response.code).json(response);
+      } catch (error: unknown) {
+         if (error instanceof CustomError) {
+            return next(new Exception(error));
+         }
+         return next(error);
       }
    }
 }
