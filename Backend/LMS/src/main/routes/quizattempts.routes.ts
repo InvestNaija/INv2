8927e335fe3e9cbd7fa296064177509ed97b3d51
AdeoluX <73498971+AdeoluX@users.noreply.github.com>
2025-02-27 10:33:47 +0100

    test will pass

--- Backend/LMS/src/main/routes/quizattempts.routes.ts
+++ Backend/LMS/src/main/routes/quizattempts.routes.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import { QuizAttemptController } from '../controllers';
-import { requireAuth } from '@inv2/common';
+import { Authentication } from '@inv2/common';
 
 class QuizAttempsRoutes {
    private router: Router;
@@ -10,10 +10,10 @@ class QuizAttempsRoutes {
    }
 
    public routes(): Router {
-      this.router.get('/', requireAuth, QuizAttemptController.getAttempts);
-      this.router.post('/start/:quiz', requireAuth, QuizAttemptController.startAttempt);
-      this.router.post('/end/:quiz', requireAuth, QuizAttemptController.endAttempt);
-      this.router.delete('/:id', requireAuth, QuizAttemptController.deleteAttempt);
+      this.router.get('/', Authentication.requireAuth, QuizAttemptController.getAttempts);
+      this.router.post('/start/:quiz', Authentication.requireAuth, QuizAttemptController.startAttempt);
+      this.router.post('/end/:quiz', Authentication.requireAuth, QuizAttemptController.endAttempt);
+      this.router.delete('/:id', Authentication.requireAuth, QuizAttemptController.deleteAttempt);
 
       return this.router;
    }
